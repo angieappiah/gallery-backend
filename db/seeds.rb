5542a4a2ce22ab5fe17acb0c7a628e32eaac9d6a
angieappiah <alegnalawn2@yahoo.com>
2021-02-02 09:12:27 -0800

    image attachment setup from AWS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
+artist = Artist.create(username: 'Angie', email: 'angie@angie.com', location: 'california')
 
-gallery = Gallery.create(style: 'mural')
+gallery = Gallery.create(style: 'mural', artist_id:'1')
 
 piece = Piece.create(name: 'The Fame Of Mine', description: 'painted overnight', gallery_id: 1)
 
-artist = Artist.create(username: 'Angie', email: 'angie@angie.com', location: 'california')
+
 
 #create one user current_user
